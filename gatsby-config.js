f9389e02c5b07254f8833a1b65d3b53ceff4447f
jiwu.wyy <jiwu.wyy@alibaba-inc.com>
2019-12-13 13:36:34 +0800

    feat: gatsby-plugin-feed output -> rss.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,57 @@ module.exports = options => {
         options,
       },
       `gatsby-plugin-react-helmet`,
-      `gatsby-plugin-twitter`,
       `gatsby-plugin-emotion`,
       `gatsby-plugin-theme-ui`,
+      {
+        resolve: `gatsby-plugin-feed`,
+        options: {
+          query: `
+            {
+              site {
+                siteMetadata {
+                  title
+                  description
+                  siteUrl
+                  site_url: siteUrl
+                }
+              }
+            }
+          `,
+          feeds: [
+            {
+              serialize: ({ query: { site, allBlogPost } }) => {
+                return allBlogPost.edges.map(edge => {
+                  return Object.assign({}, {
+                    description: edge.node.excerpt,
+                    date: edge.node.date,
+                    url: site.siteMetadata.siteUrl + edge.node.slug,
+                    guid: site.siteMetadata.siteUrl + edge.node.slug,
+                    title: edge.node.title,
+                    // custom_elements: [{ "content:encoded": edge.node.html }],
+                  })
+                })
+              },
+              query: `
+              {
+                allBlogPost(sort: {fields: date, order: DESC}) {
+                  edges {
+                    node {
+                      excerpt
+                      slug
+                      title
+                      date(formatString: "YYYY-MM-DD hh:mm:ss")
+                    }
+                  }
+                }
+              }
+              `,
+              output: "/rss.xml",
+              title: "RSS Feed",
+            },
+          ],
+        },
+      },
     ],
   }
 }
