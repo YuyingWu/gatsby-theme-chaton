1a3834b66d51053d6bc78543d9a598ba19da2514
jiwu.wyy <jiwu.wyy@alibaba-inc.com>
2020-01-12 12:48:45 +0800

    temp: comment category info in post.js

--- src/components/post.js
+++ src/components/post.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Styled, css } from "theme-ui"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 import PostFooter from "../components/post-footer"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Tags from '../components/tags'
-import Categories from '../components/categories'
+// import Categories from '../components/categories'
 
 const Post = ({
   data: {
@@ -19,32 +19,32 @@ const Post = ({
   previous,
   next,
 }) => {
-  const data = useStaticQuery(query, {
-    variables: {
-      slug: post.slug
-    }
-  });
-  const mdxData = data.allMdx.edges.filter(blog => {
-    const { node: { childMdxBlogPost : {
-      slug,
-    } } } = blog;
-    return slug === post.slug;
-  });
-  let tags = [];
-  let categories = [];
+  // const data = useStaticQuery(query, {
+  //   variables: {
+  //     slug: post.slug
+  //   }
+  // });
+  // const mdxData = data.allMdx.edges.filter(blog => {
+  //   const { node: { childMdxBlogPost : {
+  //     slug,
+  //   } } } = blog;
+  //   return slug === post.slug;
+  // });
+  let tags = post.tags || [];
+  // let categories = [];
 
-  if (mdxData.length) {
-    const { frontmatter = {} } = mdxData[0].node;
-    const { tags: frontmatterTags = [], categories: frontmatterCategories = [] } = frontmatter;
+  // if (mdxData.length) {
+  //   const { frontmatter = {} } = mdxData[0].node;
+  //   const { tags: frontmatterTags = [], categories: frontmatterCategories = [] } = frontmatter;
 
-    if (frontmatterTags.length) {
-      tags = [...frontmatterTags];
-    }
+  //   if (frontmatterTags.length) {
+  //     tags = [...frontmatterTags];
+  //   }
 
-    if (frontmatterCategories.length) {
-      categories = [...frontmatterCategories];
-    }
-  }
+  //   if (frontmatterCategories.length) {
+  //     categories = [...frontmatterCategories];
+  //   }
+  // }
   
   return (
     <Layout location={location} title={title}>
@@ -66,7 +66,7 @@ const Post = ({
 
         <Tags tags={tags}/>
 
-        <Categories categories={categories} />
+        {/* <Categories categories={categories} /> */}
 
         <MDXRenderer>{post.body}</MDXRenderer>
       </main>
@@ -77,20 +77,20 @@ const Post = ({
 
 export default Post
 
-export const query = graphql`
-query($slug: String) {
-  allMdx(filter: {childMdxBlogPost: {slug: {eq: $slug}}}) {
-    edges {
-      node {
-        childMdxBlogPost {
-          slug
-        }
-        frontmatter {
-          categories
-          tags
-        }
-      }
-    }
-  }
-}
-`
+// export const query = graphql`
+// query($slug: String) {
+//   allMdx(filter: {childMdxBlogPost: {slug: {eq: $slug}}}) {
+//     edges {
+//       node {
+//         childMdxBlogPost {
+//           slug
+//         }
+//         frontmatter {
+//           categories
+//           tags
+//         }
+//       }
+//     }
+//   }
+// }
+// `
